41907311b2256be681ad20e88af24922c2ff4e43
Maximilian Abstreiter <maximilian.abstreiter@helsinki.fi>
2024-02-02 19:38:35 +0200

    ADD part 4

---
+++ blog-list/app.js
@@ -0,0 +1,37 @@
+const express = require('express')
+const app = express()
+require('express-async-errors')
+const cors = require('cors')
+const logger = require('./utils/logger')
+const config = require('./utils/config')
+const loginRouter = require('./controllers/login')
+const usersRouter = require('./controllers/users')
+const blogsRouter = require('./controllers/blogs')
+const mongoose = require('mongoose')
+const middleware = require('./utils/middleware')
+
+mongoose.set('strictQuery', false)
+
+const url = config.MONGODB_URI
+logger.info('connecting to MongoDB')
+mongoose.connect(url)
+  .then(() => {
+    logger.info('connected to MongoDB')
+  })
+  .catch((error) => {
+    logger.error('error connecting to MongoDB:', error.message)
+  })
+
+app.use(cors())
+app.use(express.json())
+app.use(middleware.requestLogger)
+app.use(middleware.tokenExtractor)
+
+app.use('/api/login', loginRouter)
+app.use('/api/users', usersRouter)
+app.use('/api/blogs', blogsRouter)
+
+app.use(middleware.unknownEndpoint)
+app.use(middleware.errorHandler)
+
+module.exports = app
